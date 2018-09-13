df90196df2a9706902447af74b567d5f6e43c391
Stefan Warnat <ich@stefanwarnat.de>
2018-09-13 13:37:25 +0200

    Add Creator component

---
+++ src/app/creator/creator.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {Record} from "../../flexapi/models/record";
+import {ApiService} from "../../flexapi/api.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-creator',
+  templateUrl: './creator.component.html',
+  styleUrls: ['./creator.component.css']
+})
+export class CreatorComponent implements OnInit {
+    record: Record = null;
+
+    constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+      let date = new Date().toLocaleDateString();
+
+      let data = {
+          'firstname': 'Firstname API ' + date,
+          'lastname': 'Lastname API ' + date
+      };
+
+      this.apiService
+          .module('Contacts')
+          .createRecord('Contacts', data)
+          .subscribe(record => {
+              this.record = record;
+          });
+  }
+
+}
